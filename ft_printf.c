1eb5bd06f0c9afd868d5b15eda3202747d46511a
tlegrand <tlegrand@student.42lyon.fr>
2022-11-25 09:14:47 +0100

    WIP

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: tlegrand <tlegrand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/19 14:05:47 by tlegrand          #+#    #+#             */
-/*   Updated: 2022/11/19 16:46:28 by tlegrand         ###   ########.fr       */
+/*   Updated: 2022/11/24 17:59:13 by tlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,37 +27,37 @@ size_t	ft_countarg(char *str, char *set)
 }
 
 
-int	ft_select_print(char c, va_list ap)
+int	ft_select_print(char c, char *s, va_list ap)
 {
 	if (c == 'c')
 		ft_putchar_fd(va_arg(ap, char), 1);
-	if (c == 's')
+	else if (c == 's')
 		ft_putstr_fd(va_arg(ap, char *), 1);
-	if (c == 'p')
+	else if (c == 'p')
 		ft_putmem_fd(va_arg(ap, void *), 1);
-	if (c == 'd')
+	else if (c == 'd')
 		ft_putnbr_fd(va_arg(ap, int), 1);
-	if (c == 'i')
+	else if (c == 'i')
 		ft_putnbr_fd(va_arg(ap, int), 1);
-	if (c == 'u')
+	else if (c == 'u')
 		ft_putui_fd(va_arg(ap, unsigned int), 1);
-	if (c == 'x')
+	else if (c == 'x')
 		ft_puthexlow_fd(va_arg(ap, int), 1);
-	if (c == 'X')
+	else if (c == 'X')
 		ft_puthexup_fd(va_arg(ap, int), 1);
-	if (c == '%')
-		ft_putchar_fd('%', 1);
+	else 
+		ft_putchar_fd(s, 1);
 	return (0);
 }
 
-int	ft_size_format(char *str, char const *flags, char const *idset)
+int	ft_error_char(va_list ap)
 {
-	size_t	len;
+	char	c;
 
-	
+	c = (char)va_arg(ap, int);
 
-	return (0);
 }
+
 
 
 int	ft_printf(const char *str, ...)
@@ -75,9 +75,8 @@ int	ft_printf(const char *str, ...)
 		format = ft_strchr(set, (int)*(str + 1));
 		if (*str == '%' && format != NULL)
 		{
-			ft_select_print(*format, ap);
-			va_arg(ap, void);
 			str++;
+			ft_select_print(*format, str, ap);
 		}
 		else
 			ft_putchar_fd(*str, 1);
@@ -86,3 +85,18 @@ int	ft_printf(const char *str, ...)
 	va_end(ap);
 	return (0);
 }
+
+
+
+/*
+
+int	ft_size_format(char *str, char const *flags, char const *idset)
+{
+	size_t	len;
+
+	
+
+	return (0);
+}
+
+*/
