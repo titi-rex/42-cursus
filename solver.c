a7e01cbb7ed47679c79d0c725c514aae4f4a8c9b
Timothee-loup Legrand <tlegrand@z4r8p5.42lyon.fr>
2023-01-13 17:42:33 +0100

    ouinouin2

--- solver.c
+++ solver.c
@@ -6,7 +6,7 @@
 /*   By: tlegrand <tlegrand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/13 17:28:08 by tlegrand          #+#    #+#             */
-/*   Updated: 2023/01/13 17:34:57 by tlegrand         ###   ########.fr       */
+/*   Updated: 2023/01/13 17:42:28 by tlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,19 @@
 int	ft_solver(t_stack **start_a)
 {
 	t_stack	*stack_b;
-
-
+	
+	ft_quicksort(start_a, ft_stackmax(*start_a));
 	return (0);
 }
 
-int	ft_quicksort(t_stack **start_a, int len)
+void	ft_quicksort(t_stack **start_a, int len)
 {
-	
+	int	pivot;
+	int	chunk;
+
+	chunk = 1;
+	pivot = len / 4 * chunk;
+	if (pivot >= len)
+		return ;
+	while  
 }
