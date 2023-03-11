b65e51640ac208175de6245bf13ad906cc56ad9a
Louisa4124 <louisaaa14@gmail.com>
2023-03-11 21:37:45 +0100

    replace by exit status done

--- srcs/parsing.c
+++ srcs/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lboudjem <lboudjem@student.42.fr>          +#+  +:+       +#+        */
+/*   By: louisa <louisa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 11:21:07 by louisa            #+#    #+#             */
-/*   Updated: 2023/03/11 16:57:02 by lboudjem         ###   ########.fr       */
+/*   Updated: 2023/03/11 21:36:40 by louisa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ char	*ft_replace_by_exit_status(char *bloc, int i, t_line *line)
 	tmp = i + 2;
 	exit_s = ft_itoa(line->exit_status);
 	if (!exit_s)
-		return (NULL);
+		return (free(exit_s), NULL);
 	len = (int)(ft_strlen2(bloc)) - 2 + ft_strlen2(exit_s);
 	cpy = malloc((len + 1) * sizeof(char));
 	if (!cpy)
@@ -45,22 +45,14 @@ char	*ft_replace_by_exit_status(char *bloc, int i, t_line *line)
 	len = 0;
 	while (exit_s[len])
 		cpy[i++] = exit_s[len++];
-	i++;
-	printf("bloc[tmp] = %c\n", bloc[tmp]);
 	while (bloc[tmp])
-	{
-		cpy[i] = bloc[tmp];
-		i++;
-		printf("1");
-		tmp++;
-	}
+		cpy[i++] = bloc[tmp++];
 	cpy[i] = '\0';
-	printf("cpy = %s\n", cpy);
-	//free(bloc);
-	return (cpy);
+	free(bloc);
+	return (free(exit_s), cpy);
 }
 
-void	ft_handle_expansion(char *bloc, t_line *line)
+void	ft_handle_expansion(char **bloc, t_line *line)
 {
 	int		i;
 	int		name_len;
@@ -68,17 +60,17 @@ void	ft_handle_expansion(char *bloc, t_line *line)
 
 	i = 0;
 	name_len = 0;
-	while (bloc[i])
+	while ((*bloc)[i])
 	{
-		if (bloc[i] == 39)
-			while (bloc[i] != 39)
+		if ((*bloc)[i] == 39)
+			while ((*bloc)[i] != 39)
 				i++;
-		if (bloc[i] == '$' && bloc[i + 1] == '?')
-			bloc = ft_replace_by_exit_status(bloc, i, line);
-		if (bloc[i] == '$')
+		if ((*bloc)[i] == '$' && (*bloc)[i + 1] == '?')
+			*bloc = ft_replace_by_exit_status(*bloc, i, line);
+		if ((*bloc)[i] == '$')
 		{
 			i++;
-			while (bloc[i++] != ' ')
+			while ((*bloc)[i++] != ' ')
 				name_len++;
 			//bloc = ft_replace_by_value(bloc, name_len);
 		}
@@ -99,8 +91,8 @@ void	ft_browse_line(char *str, int i, int start, t_line *line)
 		if (str[i] == '|')
 		{
 			bloc = ft_creat_bloc(str, &i, &start, bloc);
+			ft_handle_expansion(&bloc, line);
 			printf("bloc = %s\n", bloc);
-			ft_handle_expansion(bloc, line);
 			free(bloc);
 		}
 		if (str[i] == '\0' || str[i + 1] == '\0')
