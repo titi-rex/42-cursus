2f1c4ee3b27e70641b7bf5bbb054554c10944af8
reina <reina@wunder>
2023-03-11 19:54:19 +0100

    wip

--- srcs/main_test.c
+++ srcs/main_test.c
@@ -6,7 +6,7 @@
 /*   By: tlegrand <tlegrand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 12:28:13 by tlegrand          #+#    #+#             */
-/*   Updated: 2023/03/11 16:51:11 by tlegrand         ###   ########.fr       */
+/*   Updated: 2023/03/11 19:51:50 by tlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ int	main(int ac, char **arg)
 		{
 			dprintf(2, "he heo stop\n");
 			g_sig = 0;
+			exit(0);
 		}
 	}
 
