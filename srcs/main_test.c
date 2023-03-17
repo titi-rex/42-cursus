4bceb6c22809e370661a97786857896e8758b262
Timothee-loup Legrand <tlegrand@z4r8p5.42lyon.fr>
2023-03-17 16:17:54 +0100

    ouin je deteste merge sur vscode

--- srcs/main_test.c
+++ srcs/main_test.c
@@ -6,11 +6,7 @@
 /*   By: tlegrand <tlegrand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 12:28:13 by tlegrand          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2023/03/16 17:23:28 by tlegrand         ###   ########.fr       */
-=======
-/*   Updated: 2023/03/17 15:30:34 by tlegrand         ###   ########.fr       */
->>>>>>> loulouleretour
+/*   Updated: 2023/03/17 16:17:27 by tlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -75,7 +71,7 @@ int	fmain(int ac, char **arg, char **env)
 	return (0);
 }
 
-int main(int ac, char **arg, char **env)
+int	main(int ac, char **arg, char **env)
 {
 	char	*input;
 	t_line	line;
@@ -97,30 +93,7 @@ int main(int ac, char **arg, char **env)
 		{
 			printf("exit\n");
 			ft_clean_exit(&line, line.exit_status);
-<<<<<<< HEAD
 		}
-		//parsing(&cmdline, line);
-		line.cmd = ft_calloc(1, sizeof(t_cmd));
-		if (!line.cmd)
-			return (ft_perror_return_int(NULL));
-		s_cmd_init(line.cmd);
-		line.cmd->arg = ft_split(input, ' ');
-		if (!line.cmd->arg[0])
-			line.n_cmds = 0;
-		else
-			line.n_cmds = 1;
-		if (!ft_strncmp(input, "code", 5))
-			printf("g_status act is %d\n", g_status);
-		else if (!ft_strncmp(input, "here", 5) || !ft_strncmp(input, "\"here\"", 7))
-		{
-			printf("delimiter : %s\n", input);
-			buff = ft_here_doc_mode(input);
-			printf("here_doc :\n%s\n", buff);
-			free(buff);
-		}
-		else//if (line.cmd->arg && line.cmd->arg[0])
-			ft_exe_master(&line);
-=======
 		ft_browse_line(input, 0, 0, &line);
 		free(input);
 		input = NULL;
@@ -129,7 +102,6 @@ int main(int ac, char **arg, char **env)
 		//	printf("g_status act is %d\n", g_status);
 		ft_exe_master(&line);
 		//dprintf(2, "inout : %s\n", input);
->>>>>>> loulouleretour
 		s_line_reset(&line);
 	}
 	(void) arg;
