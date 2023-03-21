d16d69b6cd994acdf075735799e8444e45a8d817
reina <reina@wunder>
2023-03-21 20:38:07 +0100

    essaye de supp ca s itu peux

---
+++ srcs/structure/s_cmd_operator.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   s_cmd_operator.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tlegrand <tlegrand@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/10 22:50:22 by tlegrand          #+#    #+#             */
+/*   Updated: 2023/03/21 20:34:35 by tlegrand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+t_cmd	*ft_cmd_last(t_cmd *cmd)
+{
+	if (cmd)
+	{
+		while (cmd->next)
+			cmd = cmd->next;
+	}
+	return (cmd);
+}
+
+t_cmd	*ft_cmd_first(t_cmd *cmd)
+{
+	if (cmd)
+	{
+		while (cmd->previous)
+			cmd = cmd->previous;
+	}
+	return (cmd);
+}
+
+void	ft_cmd_clear_lst(t_cmd	**cmd)
+{
+	t_cmd	*tmp;
+	t_cmd	*idx;
+
+	idx = (*cmd)->next;
+	while (*cmd)
+	{
+		tmp = (*cmd)->previous;
+		ft_cmd_del(*cmd);
+		*cmd = tmp;
+	}
+	*cmd = idx;
+	while (*cmd)
+	{
+		tmp = (*cmd)->next;
+		ft_cmd_del(*cmd);
+		*cmd = tmp;
+	}
+}
+
+void	ft_cmd_add_back(t_cmd **start, t_cmd *new)
+{
+	t_cmd	*tmp;
+
+	if (!start || !new)
+		return ;
+	if (!*start)
+		*start = new;
+	else
+	{
+		tmp = ft_cmd_last(*start);
+		tmp->next = new;
+		new->previous = tmp;
+	}
+}
+
+void	s_cmd_init(t_cmd *cmd)
+{
+	cmd->arg = NULL;
+	cmd->io = NULL;
+	cmd->next = NULL;
+	cmd->previous = NULL;
+}
