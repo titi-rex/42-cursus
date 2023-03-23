3d5bfec70baccd70b094af4dae6fae309a9ce909
tlegrand <tlegrand@z4r1p5.42lyon.fr>
2023-03-23 17:40:27 +0100

    clean

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: tlegrand <tlegrand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 12:28:13 by tlegrand          #+#    #+#             */
-/*   Updated: 2023/03/23 17:38:09 by tlegrand         ###   ########.fr       */
+/*   Updated: 2023/03/23 17:40:12 by tlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -68,7 +68,7 @@ int	main(int ac, char **arg, char **env)
 		err = ft_browse_line(input, 0, 0, &line);
 		free(input);
 		if (err != 0)
-			dprintf(2, "Error parsing \n");
+			printf("Error parsing \n");
 		else if (!(g_status & INTERRUPT))
 			ft_exe_master(&line);
 		s_line_reset(&line);
