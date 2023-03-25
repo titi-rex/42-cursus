f79cce557b0ce89a58f5dae7f3820bccc78e0a2b
Louisa4124 <louisaaa14@gmail.com>
2023-03-25 21:06:23 +0100

    FIX : parsing export

--- srcs/parsing/parsing_export.c
+++ srcs/parsing/parsing_export.c
@@ -6,26 +6,67 @@
 /*   By: louisa <louisa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/21 10:33:20 by lboudjem          #+#    #+#             */
-/*   Updated: 2023/03/25 19:05:52 by louisa           ###   ########.fr       */
+/*   Updated: 2023/03/25 21:05:45 by louisa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
-char	*ft_handle_export(char *bloc, t_line *line)
+int	nb_quotes(char *bloc)
 {
+	int	nb;
 	int	i;
 
+	nb = 0;
 	i = 0;
-    while (bloc && bloc[i])
+	while (bloc[i])
 	{
-		if (ft_strncmp(bloc, "export", 6) == 0)
-		{
-			bloc = ft_del_quotes(bloc, 0, 0);
-			bloc = ft_handle_expansion(bloc, line);
-			return (bloc);
-		}
+		if (bloc[i] == 39)
+			nb++;
 		i++;
+	}
+	return (nb);
+}
+
+char	*ft_protect_export_quotes(char *bloc)
+{
+	char	*cpy;
+	int		size;
+	int		i;
+	int		j;
+
+	cpy = NULL;
+	i = 0;
+	j = 0;
+	size = ft_strlen2(bloc) + (nb_quotes(bloc) * 2);
+	cpy = malloc((size + 1) * sizeof(int));
+	if (!cpy)
+		return NULL;
+	while (bloc[i])
+	{
+		if (bloc[i] != 39)
+			cpy[j] = bloc[i];
+		if (bloc[i] == 39)
+		{
+			cpy[j] = 34;
+			cpy[j + 1] = 39;
+			cpy[j + 2] = 34;
+			j += 2;
+		}
+		j++;
+		i++;
+	}
+	return (cpy);
+}
+
+char	*ft_handle_export(char *bloc, t_line *line)
+{
+	if (ft_strncmp(bloc, "export", 6) == 0)
+	{
+		bloc = ft_del_quotes(bloc, 0, 0);
+		bloc = ft_handle_expansion(bloc, line);
+		//bloc = ft_protect_export_quotes(bloc);
+		return (bloc);
 	}
 	return (bloc);
 }
