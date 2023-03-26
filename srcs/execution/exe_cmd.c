0682f06ab19ce0deb20181d2b20bff6967308539
reina <reina@wunder>
2023-03-26 15:58:05 +0200

    end tititest

--- srcs/execution/exe_cmd.c
+++ srcs/execution/exe_cmd.c
@@ -6,7 +6,7 @@
 /*   By: tlegrand <tlegrand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 15:07:58 by tlegrand          #+#    #+#             */
-/*   Updated: 2023/03/26 15:22:26 by tlegrand         ###   ########.fr       */
+/*   Updated: 2023/03/26 15:51:48 by tlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -129,7 +129,6 @@ void	ft_exe_master(t_line *line)
 	g_status |= EXECUTION;
 	while (i < line->n_cmds)
 	{
-		debug_print_cmd_arg(line->cmd->arg);
 		if (line->cmd->next)
 		{
 			if (pipe(line->pipe[i % 2]) == -1)
