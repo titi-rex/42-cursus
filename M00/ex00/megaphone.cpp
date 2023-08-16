3de6d471beadf2dc421d31ef879e8ee661a1ec74
tlegrand <titirex@pm.me>
2023-08-17 00:14:37 +0200

    clean

--- M00/ex00/megaphone.cpp
+++ M00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: tlegrand <tlegrand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 11:50:48 by tlegrand          #+#    #+#             */
-/*   Updated: 2023/08/16 20:38:48 by tlegrand         ###   ########.fr       */
+/*   Updated: 2023/08/17 00:13:58 by tlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,5 @@ int	main(int c, char **arg)
 	for (int i = 0; i < str.length(); ++i)
 		str.at(i) = static_cast<char>(std::toupper(str.at(i)));
 	std::cout << str << std::endl;
-	std::cout << std::setw(10) << std::setfill('x') << 1323 << std::endl;
-	std::cout << 1323 << std::endl;
-	std::cout << std::setw(10) <<  1323 << std::endl;
 	return (0);
 }
