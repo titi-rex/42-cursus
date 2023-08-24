d946557ced0e7644baac1bda451caed7be096271
titirex <reina@pearl>
2023-08-24 15:23:08 +0200

    ADD : m00 ex04 finished

--- M01/ex04/main.cpp
+++ M01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tlegrand <tlegrand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/21 20:41:42 by tlegrand          #+#    #+#             */
-/*   Updated: 2023/08/24 14:37:31 by tlegrand         ###   ########.fr       */
+/*   Updated: 2023/08/24 15:22:13 by tlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,65 +38,55 @@ file.seekg( 0, std::ios_base::beg );
 #include <fstream>
 #define BUFFER_SIZE 1024
 
-std::string	get_file(char *pathanme)
+int	get_file(const char* const pathanme, std::string& str_file)
 {
 	std::ifstream	in;
-	std::string		str_file;
 	char			buffer[BUFFER_SIZE];
 	
 	in.open(pathanme, std::ios::in);
-	
+	if (in.good() == false)
+	{
+		in.close();
+		return (1);
+	}
 	while (!in.eof())
 	{
 		in.get(buffer, BUFFER_SIZE, '\0');
 		str_file.append(buffer);
 	}
 	in.close();
-
-	return (str_file);
+	return (0);
 }
 
-void	write_file(char *pathname, std::string data)
+int	write_file(char *pathname, std::string data)
 {
 	std::ofstream	out;
 	std::string		name(pathname);
 	
 	name += ".replace";
 	out.open(name.data(), std::ios::out);
+	if (out.good() == false)
+	{
+		out.close();	
+		return (1);
+	}
 	out << data;
 	out.close();
-}
-int	stringContent(const std::string small, const std::string big)
-{
-	std::size_t		i = 0;
-	unsigned int	k = 0;
-	
-	if (small.size() > big.size())
-		return (-1);
-	while (k < big.size() - small.size())
-	{
-		i = 0;	
-		while (small.at(i) && big.at(k + i) && small.at(i) == big.at(k + i))
-			++i;
-		if (i == small.size())
-			return (k);
-		++k;
-	}
-	return (-1);
+	return (0);
 }
 
-void	search_n_destroy(std::string data, char *s1, char *s2)
+void	search_n_destroy(std::string& data, const char *s1, const char *s2)
 {
-	std::string	search(s1);
-	std::string replace(s2);
-	std::size_t	i = 0;
+	const std::string	search(s1);
+	const std::string	replace(s2);
+	std::size_t			i = 0;
 	
 	while (i < data.size())
 	{
-		if (data.compare(i, std::string::npos, search))
+		if (data.compare(i, search.size(), search) == 0)
 		{
-			std::cout << search << " is in " << data << " at i = " << i << std::endl;
-			// data.erase();
+			data.erase(i, search.size());
+			data.insert(i, replace);
 		}
 		++i;
 	}
@@ -108,9 +98,10 @@ int	main(int ac, char** arg)
 		return (1);
 	std::string		buff;
 
-	buff = get_file(arg[1]);
-	
+	if (get_file(arg[1], buff))
+		return (1);
 	search_n_destroy(buff, arg[2], arg[3]);
-	write_file(arg[1], buff);
+	if (write_file(arg[1], buff))
+		return (1);
 	return (0);	
 }
