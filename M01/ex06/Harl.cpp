41dab369944a821190993c829e00a9ea7a06740e
reina <reina@wunder>
2023-09-10 00:26:51 +0200

    clean

--- M01/ex06/Harl.cpp
+++ M01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: tlegrand <tlegrand@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/09 22:34:03 by tlegrand          #+#    #+#             */
-/*   Updated: 2023/09/10 00:18:15 by tlegrand         ###   ########.fr       */
+/*   Updated: 2023/09/10 00:26:10 by tlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ Harl::Harl(void) {std::cout << "Harl constructor called" << std::endl;};
 Harl::~Harl(void) {std::cout << "Harl destructor called" << std::endl;};
 
 
-void	Harl::_debug(void) {std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;};
+void	Harl::_debug(void) const {std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;};
 	
-void	Harl::_info(void) {std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;};
+void	Harl::_info(void) const {std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;};
 	
-void	Harl::_warning(void) {std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;};
+void	Harl::_warning(void) const {std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;};
 	
-void	Harl::_error(void) {std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl; };
+void	Harl::_error(void) const {std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl; };
 
-void	Harl::complain(int start) 
+void	Harl::complain(int start) const
 {
 	std::string	str[4] = {"DEBUG", "INFO","WARNING", "ERROR"};
 	hptr		func[4] = {&Harl::_debug, &Harl::_info, &Harl::_warning, &Harl::_error};
@@ -39,7 +39,7 @@ void	Harl::complain(int start)
 	}
 };
 
-void	Harl::filter(std::string level) 
+void	Harl::filter(std::string level) const
 {
 	std::string	str[5] = {"DEBUG", "INFO","WARNING", "ERROR", "Probably complaining about insignificant problems"};
 	int			n;
