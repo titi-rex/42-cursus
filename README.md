d6f5c6e980447ca4177375fb515eb79791d10f94
reina <reina@wunder>
2023-09-10 22:35:20 +0200

    ADD : v2, handle corrrectly composed type, respect orthodox canonical form and other stuff

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # cpp_class_gen
 cpp class generator script <br>
 This script help you generate your cpp class <br>
-Usage : ./class.sh newClass or ./class.sh <br>
+Usage : ./class-gen.sh newClass or ./class-gen.sh <br>
 <br>
+chmod +x class-gen.sh && cp class-gen.sh ~/bin/ && alias class="~/bin/class-gen.sh" <br>
+<br>
+add ~/bin to your path and eventually make a permanent alias <br>
--- class-gen.sh
+++ class-gen.sh
@@ -7,22 +7,69 @@
 #add padding depending on longer variable type knew
 padding () {
 	PAD=""
-	if [[ ${#arr[0]} -ge $SIZE  ]];
+	if [[ ${#TYPE} -ge $SIZE  ]];
 	then
-		SIZE=$(( (${#arr[0]} / 4) * 4 + 4))
+		SIZE=$(( (${#TYPE} / 4) * 4 + 4))
 	fi
-	if [[ ${#arr[0]} -lt $SIZE ]];
+	if [[ ${#TYPE} -lt $SIZE ]];
 	then
-		for (( i=${#arr[0]}; i < $SIZE; i+=4))
+		for (( i=${#TYPE}; i < $SIZE; i+=4))
 		do
 			PAD="$PAD	"
 		done
 	fi
 }
 
-#padding function
+#get type of variable
+get_type_var () {
+	TYPE="${arr[0]}"
+	for ((i=1; i<${#arr[@]} - 1; ++i))
+	do
+		TYPE="$TYPE ${arr[i]}"
+	done
+}
+
+#adapt TYPE and STATIC dpending on static var presence
+adapt_static () {
+	if [[ ${arr[0]} == "static" ]];
+	then
+		STATIC="static "
+		TYPE="${arr[1]}"
+		for ((i=2; i<${#arr[@]} - 1; ++i))
+		do
+			TYPE="$TYPE ${arr[i]}"
+		done
+	else
+		STATIC=""
+	fi
+}
+
+#get start of function
+get_start_func () {
+	START=""
+	for (( i=0; i < ${#arr[@]}; ++i ))
+	do
+		if [[ ${arr[$i]} =~ [()] ]];
+		then
+			START=$i
+			break ;
+		fi
+	done
+}
+
+#get return type of function
+get_type_func () {
+	TYPE="${arr[0]}"
+	for (( i=1; i < $START; ++i ))
+	do
+		TYPE="$TYPE ${arr[$i]}"
+	done
+}
+
+#get function
 get_function () {
-	for (( i=1; i < ${#arr[@]}; ++i ))
+	FUNC=""
+	for (( i=$START; i < ${#arr[@]}; ++i ))
 	do
 		FUNC="$FUNC${arr[$i]}"
 		if [ $i -ne $(( ${#arr[@]} - 1 )) ];
@@ -57,7 +104,7 @@ fi
 printf "Hint : \033[3mYou can press enter or write 'next' (or 'n') to ignore next questions\033[0m\n"
 printf "\033[4mExpected format :\033[0m\n"
 printf "Include : \033[3minclude\033[0m or \033[3m\"include\"\033[0m for local file\n"
-printf "Variable :\033[3m type name \033[0m (example : int number)\n"
+printf "Variable :\033[3m type name \033[0m (example : int* number) \033[0;31m(please do not stick * or & to name)\033[0m\n"
 printf "Function :\033[3m type name(type1 var1, ..., typen varn) \033[0m (example : void hello(void) const)\n"
 
 
@@ -68,7 +115,6 @@ _TMP="$NAME.tmp"
 
 
 #create include guard
-INCLUDE_GUARD="_"
 for (( i=0; i<${#NAME}; ++i))
 do
 
@@ -83,12 +129,10 @@ INCLUDE_GUARD="${INCLUDE_GUARD^^}_H__"
 
 
 #write start of files
-echo "
-#ifndef $INCLUDE_GUARD
+echo "#ifndef $INCLUDE_GUARD
 # define $INCLUDE_GUARD" > $_NAME_HPP
 
-echo "
-#include \""$_NAME_HPP"\"" > $_NAME_CPP
+echo "#include \""$_NAME_HPP"\"" > $_NAME_CPP
 
 
 #ask for include file
@@ -113,17 +157,8 @@ do
 done
 
 
-#add default constructor and destructor in cpp file
-echo "
-$NAME::$NAME(void) {};
-
-$NAME::~$NAME(void) {};
-
-" >> $_NAME_CPP
-
-
 #write private section
-printf "\033[0;31m!---PRIVATE SECTION---!\033[0m\n"
+printf "\033[1;31m!---PRIVATE SECTION---!\033[0m\n"
 printf "\033[3mAutomatic '_' prefix\033[0m\n"
 
 echo "
@@ -142,29 +177,35 @@ do
 		break
 	fi
 
+	get_type_var
 	padding
+	adapt_static
 
 	#uppercase first letter to match camelCase
-	CVAR=${arr[1]}
+	NVAR=${arr[${#arr[@]} - 1]}
+	CVAR=$NVAR
 	CVAR=${CVAR^}
 
 	GET="get"$CVAR"(void) const"
-	SET="set"$CVAR"(${arr[0]} ${arr[1]})"
+	SET="set"$CVAR"($TYPE $NVAR)"
 
 	#write private variable with padding and '-' prefix
-	echo "		${arr[0]}""$PAD""_${arr[1]};" >> $_NAME_HPP
+	echo "		$STATIC$TYPE""$PAD""_$NVAR;" >> $_NAME_HPP
 
 	#store get and set function int mp file int order to write it latter in public section
-	echo "		${arr[0]}	$GET;" >> $_TMP
-	echo "		void	$SET;" >> $_TMP
+	TMP_HPP="${TMP_HPP}		$TYPE	$GET;\n"
+	TMP_HPP="${TMP_HPP}		void	$SET;\n"
 
-	#write get and set in cpp file
-	echo "${arr[0]}	$NAME::$GET { return (this->_${arr[1]}); };
-	" >> $_NAME_CPP
-	echo "void	$NAME::$SET { this->_${arr[1]} = ${arr[1]}; };
-	" >> $_NAME_CPP
+	#store get and set funciton 
+	FUNC_PROTO="${FUNC_PROTO}$TYPE	$NAME::$GET { return (this->_$NVAR); };\n"
+	FUNC_PROTO="${FUNC_PROTO}void	$NAME::$SET { this->_$NVAR = $NVAR; };\n\n"
+	#store for constructor and operator=
+	CONS="${CONS}_$NVAR(0), "
+	OPEG="${OPEG}this->set${CVAR}(cpy.get${CVAR}());
+	"
 
 done
+
 
 #ask for private function
 SIZE=4
@@ -176,14 +217,22 @@ do
 		break
 	fi
 
+	get_start_func
+
+	if [[ $START == "" ]];
+	then
+		printf "Wrong function format \n"
+		continue ;
+	fi
+
+	get_type_func
 	padding
 	get_function
 
 	#write function protoype in hpp and cpp files
-	echo "		${arr[0]}""$PAD""_$FUNC;" >> $_NAME_HPP
-	echo "${arr[0]}	$NAME::_$FUNC {};
-	" >> $_NAME_CPP
-	FUNC=""
+	echo "		$TYPE""$PAD""_$FUNC;" >> $_NAME_HPP
+	#store function protoype
+	FUNC_PROTO="${FUNC_PROTO}$TYPE	$NAME::_$FUNC {};\n\n"
 
 done
 
@@ -195,9 +244,9 @@ echo "
 	public	:" >> $_NAME_HPP
 
 
-#ask for public  variable
+#ask for public variable
 SIZE=4
-while read -a arr -p "Enter public  variable : ";
+while read -a arr -p "Enter variable : ";
 do
 
 	if [[ ( ${arr[0]} = "" ) || ( ${arr[0]} = "n" ) || ( ${arr[0]} = "next" ) ]];
@@ -205,27 +254,55 @@ do
 		break
 	fi
 
+	get_type_var
 	padding
+	adapt_static
 
+	NVAR=${arr[${#arr[@]} - 1]}
 	#write variable in hpp file
-	echo "		${arr[0]}""$PAD""${arr[1]};" >> $_NAME_HPP
+	echo "		$TYPE""$PAD""$NVAR;" >> $_NAME_HPP
+	#store for constructor and operator=
+	CONS="${CONS}$NVAR(0), "
+	OPEG="${OPEG}this->$NVAR = cpy.$NVAR;
+	"
 
 done
 
 
 #add default constructor and destructor in hpp file
-echo "
+echo -e "
 		$NAME(void);
+		$NAME(const $NAME& cpy);
+		$NAME&	operator=(const $NAME& cpy);
 		~$NAME(void);
+
+$TMP_HPP
 " >> $_NAME_HPP
 
 
-#copy get and set function prototype in hpp file and add newline
-cat $_TMP >> $_NAME_HPP
-echo "" >> $_NAME_HPP
+#add default constructor and destructor in cpp file
+CONS=${CONS:0:${#CONS[@]} - 3}
+echo -e "
+$NAME::$NAME(void) : $CONS {};
+
+$NAME::$NAME(const $NAME& cpy)
+{
+	${OPEG}
+};
+
+$NAME&	$NAME::operator=(const $NAME& cpy) 
+{
+	if (this == &cpy)
+		return (*this);
+	${OPEG}return (*this);
+};
+
+$NAME::~$NAME(void) {};
+
+$FUNC_PROTO" >> $_NAME_CPP
 
 
-#aask for public functions
+#ask for public functions
 SIZE=4
 while read -a arr -p "Enter public function : ";
 do
@@ -235,6 +312,15 @@ do
 		break
 	fi
 
+	get_start_func
+
+	if [[ $START == "" ]];
+	then
+		printf "Wrong function format \n"
+		continue ;
+	fi
+
+	get_type_func
 	padding
 	get_function
 
@@ -242,9 +328,9 @@ do
 	echo "		${arr[0]}""$PAD""$FUNC;" >> $_NAME_HPP
 	echo "${arr[0]}	$NAME::$FUNC {};
 	" >> $_NAME_CPP
-	FUNC=""
 
 done
+
 
 #write end of hpp file
 echo "};
