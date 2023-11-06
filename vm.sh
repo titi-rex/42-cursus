da4baa9c46c31ce7de7dd58698bfc9d4f4e01215
Timothee-loup Legrand <tlegrand@z4r6p7.42lyon.fr>
2023-11-06 14:51:24 +0100

    ADD : start script vm

---
+++ vm.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-install update -y && sudo apt upgrade -y
+parametrer ssh
+connecter en local via ssh
+
+parameter sudo
+
+
+install docker
+install make
+install git
+
+
+
+
+
+
